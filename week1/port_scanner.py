6b766755c6eca9fc869898d1307b1ee941a6ec8a
Shreyas Kumbhar <shreyask612@gmail.com>
2026-04-04 11:35:24 +0530

    Week1 Day5: Updated port_scanner code

--- week1/port_scanner.py
+++ week1/port_scanner.py
@@ -1,20 +1,36 @@
-
 import socket
 
 def scan_ports(target, start_port, end_port):
-    print(f"Scanning {target}...")
+    try:
+        target_ip = socket.gethostbyname(target)
+    except socket.gaierror:
+        print("Invalid hostname.")
+        return
+
+    print(f"Scanning {target} ({target_ip})...\n")
     open_ports = []
 
-    for port in range(start_port, end_port + 1):
-        s = socket.socket()
-        s.settimeout(0.5)
-        result = s.connect_ex((target, port))
-        if result == 0:
-            print(f"  [OPEN] Port {port}")
-            open_ports.append(port)
-        s.close()
+    try:
+        for port in range(start_port, end_port + 1):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(0.5)
 
-    print(f"Scan complete. {len(open_ports)} open ports found.")
+            try:
+                result = s.connect_ex((target_ip, port))
+                if result == 0:
+                    print(f"[OPEN] Port {port}")
+                    open_ports.append(port)
+            except socket.error:
+                pass
+            finally:
+                s.close()
+
+    except KeyboardInterrupt:
+        print("\nScan interrupted.")
+        return
+
+    print(f"\nScan complete. {len(open_ports)} open ports found.")
     return open_ports
 
-scan_ports("127.0.0.1", 1, 1024)
+
+scan_ports("127.0.0.1", 1, 10000)
